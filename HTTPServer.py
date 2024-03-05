6b59b620a86fd925a686dbc6a69607f292073df6
Elin <75596885+ElinLiu0@users.noreply.github.com>
2024-03-05 20:58:49 +0800

    Add files via upload

--- HTTPServer.py
+++ HTTPServer.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from utils.generation import SAMPLE_RATE, generate_audio, preload_models,generate_audio_from_long_text
 import uvicorn
-import json
+from typing import Optional
 from scipy.io.wavfile import write as write_wav
 import pathlib
 import subprocess
@@ -39,9 +39,14 @@ class RequestBody(BaseModel):
     language:str
     noaccent:bool
     longprompt:bool
+
+class ResponseBody(BaseModel):
+    audioURL: Optional[str]
+    code: Optional[int]
+    error: Optional[str]
 print("Setting up routes...")
 @app.post("/generate")
-async def generateAudio(request:RequestBody):
+async def generateAudio(request:RequestBody) -> ResponseBody:
     textPrompt = request.textPrompt
     character = request.character
     language = request.language
@@ -49,29 +54,26 @@ async def generateAudio(request:RequestBody):
     longpromptMode = request.longprompt
     executionMode = "long" if longpromptMode else "short"
     if language not in lang2accent.keys():
-        return {"error":"Language not support","code":"404"}
+        return ResponseBody(error="Language not supported",code=404)
     if not pathlib.Path(f"./presets/{character}.npz").exists():
-        return {"error":"Character not found","code":"404"}
+        return ResponseBody(error="Character not found",code=404)
     if not pathlib.Path(f"./cache/{language}/{character}/{executionMode}").exists():
         pathlib.Path(f"./cache/{language}/{character}/{executionMode}").mkdir(parents=True,exist_ok=True)
-    if pathlib.Path(f"./cache/{language}/{character}/{textPrompt}.wav").exists():
-        return {"audioURL":f"http://localhost:8080/{language}/{character}/{executionMode}/{textPrompt}.wav","code":"200"}
+    if pathlib.Path(f"./cache/{language}/{character}/{executionMode}/{textPrompt}.wav").exists():
+        return ResponseBody(audioURL=f"http://localhost:8080/{language}/{character}/{executionMode}/{textPrompt}.wav",code=200)
     else:
-        if "AOE" in textPrompt and language == "ja":
-            textPrompt = "範囲傷害です"
-        else:
-            try:
-                if longpromptMode:
-                    audio_array = generate_audio_from_long_text(textPrompt, prompt=character,language=language,accent=lang2accent[language] if noaccent == False else "no-accent")
-                else:
-                    audio_array = generate_audio(textPrompt, prompt=character,language=language,accent=lang2accent[language] if noaccent == False else "no-accent")
-                # Saving a cache
-                write_wav(f"./cache/{language}/{character}/{executionMode}/{textPrompt}.wav",SAMPLE_RATE,audio_array)
-                torch.cuda.synchronize() # 同步所有的GPU Stream
-                torch.cuda.empty_cache() # 清空所有的GPU缓存
-            except Exception as e:
-                return {"error":str(e),"code":"500"}
-    return {"audioURL":f"http://localhost:8080/{language}/{character}/{executionMode}/{textPrompt}.wav","code":"200"}
+        try:
+            if longpromptMode:
+                audio_array = generate_audio_from_long_text(textPrompt, prompt=character,language=language,accent=lang2accent[language] if noaccent == False else "no-accent")
+            else:
+                audio_array = generate_audio(textPrompt, prompt=character,language=language,accent=lang2accent[language] if noaccent == False else "no-accent")
+            # Saving a cache
+            write_wav(f"./cache/{language}/{character}/{executionMode}/{textPrompt}.wav",SAMPLE_RATE,audio_array)
+            torch.cuda.synchronize() # 同步所有的GPU Stream
+            torch.cuda.empty_cache() # 清空所有的GPU缓存
+        except Exception as e:
+            return ResponseBody(error=str(e),code=500)
+    return ResponseBody(audioURL=f"http://localhost:8080/{language}/{character}/{executionMode}/{textPrompt}.wav",code=200)
 if __name__ == "__main__":
     print("Starting server...")
     uvicorn.run(app,host="localhost",port=8000)
